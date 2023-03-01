233dc41fe47545b16c606553f3e1a93c3f71ef17
ADMIN <ADMIN@DESKTOP-6F2N9R8>
2023-03-01 10:13:02 +0700

    Add project files.

---
+++ ASM2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ASM2.Data;
+using ASM2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace ASM2.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*private readonly ILogger<HomeController> _logger;*/
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public HomeController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            webHostEnvironment = hostEnvironment;
+
+        }
+
+        public IActionResult Index(string searchString)
+        {
+            var books = from l in _context.Books select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(books);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
